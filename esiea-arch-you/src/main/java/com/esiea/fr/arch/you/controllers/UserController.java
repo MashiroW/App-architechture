28196d0e2f08105e90292411f2446d601db6f4f3
YouYou <47890166+MashiroW@users.noreply.github.com>
2022-01-10 22:17:10 +0100

    Add files via upload

--- esiea-arch-you/src/main/java/com/esiea/fr/arch/you/controllers/UserController.java
+++ esiea-arch-you/src/main/java/com/esiea/fr/arch/you/controllers/UserController.java
@@ -1,11 +1,16 @@
 package com.esiea.fr.arch.you.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +18,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.esiea.fr.arch.you.dto.UserDto;
 import com.esiea.fr.arch.you.entity.User;
+import com.esiea.fr.arch.you.exceptions.BadCredentialsException;
+import com.esiea.fr.arch.you.exceptions.ResourceNotFoundException;
 import com.esiea.fr.arch.you.repository.UserRepository;
 import com.esiea.fr.arch.you.service.UserService;
 
@@ -44,10 +51,10 @@ public class UserController {
     }
 	
 	//ADDING AN USER
-    @RequestMapping(value = "/api/v1/adduser", method = RequestMethod.POST, consumes =MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/api/v1/users/add", method = RequestMethod.POST, consumes =MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<UserDto> createUser(@RequestBody UserDto user) 
     {
-    	userService.createOrUpdateUser(user);
+    	userService.createUser(user);
     	return new ResponseEntity<UserDto>(user, HttpStatus.OK);    
     }
     
@@ -56,5 +63,39 @@ public class UserController {
     public ResponseEntity<Iterable<User>> getAllUsers(){
         return  ResponseEntity.ok().body(userRepository.findAll());
     }
+    
+    //Get user by id
+    @GetMapping("/api/v1/users/get/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable(value = "id") Long userId) throws ResourceNotFoundException{
+    	User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User not found for this id: "+userId));
+    	return ResponseEntity.ok().body(user);
+    }
+    
+    //Delete user
+    @DeleteMapping("/api/v1/users/delete/{id}")
+    public ResponseEntity<Map<String,Boolean>> deleteUser(@PathVariable(value = "id") Long id) throws ResourceNotFoundException {
+    	User user = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User not found for this id: "+id));
+    	userRepository.delete(user);
+    	System.out.println("The user "+user.getFirstname()+" "+user.getLastname()+" (ID = "+user.getId()+") has been removed");
+    	Map<String, Boolean> response = new HashMap<>();
+    	response.put("deleted", Boolean.TRUE);
+    	return ResponseEntity.ok(response);
+    }
+    
+    //Login
+    @RequestMapping(value = "/api/v1/users/login", method = RequestMethod.POST)
+    public ResponseEntity<UserDto> loginUser(@RequestBody UserDto user) throws BadCredentialsException{
+    	String tempUsername = user.getUsername();
+    	String tempPassword = user.getPassword();
+    	UserDto userObj = null;
+    	if (tempUsername != null && tempPassword != null) {
+    		userObj = userService.fetchUserByUsernameAndPassword(tempUsername, tempPassword);   		
+    	}
+    	if (userObj == null) {
+    		throw new BadCredentialsException("Bad credentials");
+    	}
+    	return new ResponseEntity<UserDto>(userObj, HttpStatus.OK);    	
+    }
         
 }
+
--- esiea-arch-you/src/main/java/com/esiea/fr/arch/you/service/UserService.java
+++ esiea-arch-you/src/main/java/com/esiea/fr/arch/you/service/UserService.java
@@ -8,6 +8,7 @@ import com.esiea.fr.arch.you.entity.User;
 import com.esiea.fr.arch.you.mapper.MapperUser;
 import com.esiea.fr.arch.you.repository.UserRepository;
 
+
 @Service
 public class UserService {
 	
@@ -17,9 +18,16 @@ public class UserService {
 	@Autowired
 	private MapperUser mapperUser;
 	
-	public void createOrUpdateUser(UserDto userDto){
+	
+	public void createUser(UserDto userDto){
 		User user = mapperUser.map(userDto);
 		userRepository.save(user);
+		
+		System.out.println("The user "+user.getFirstname()+" "+user.getLastname()+" (ID = "+user.getId()+") has been added");
+	}
+	
+	public UserDto fetchUserByUsernameAndPassword(String username, String password) {
+		return userRepository.findByUsernameAndPassword(username,password);
 	}
 	
 }
